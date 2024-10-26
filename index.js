2aa095d9ba3fbb83a8b364790b3d07da34a12406
tolujana <tolujana@gmail.com>
2024-10-26 19:26:54 +0100

    added headless to index

--- index.js
+++ index.js
@@ -5,5 +5,23 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
+import PushNotification from 'react-native-push-notification';
+import configureBackgroundFetch from './src/util/BackgroundTask';
+import BackgroundFetch from 'react-native-background-fetch';
 AppRegistry.registerComponent(appName, () => App);
+
+PushNotification.configure({
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  popInitialNotification: true,
+  requestPermissions: true,
+});
+
+const headlessTask = async event => {
+  console.log('[BackgroundFetch HeadlessTask] event: ', event.taskId);
+  await configureBackgroundFetch();
+  BackgroundFetch.finish(event.taskId);
+};
+
+BackgroundFetch.registerHeadlessTask(headlessTask);

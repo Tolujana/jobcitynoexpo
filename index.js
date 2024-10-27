c80d486307ac2020398cbef01115d7a3e2bdc8b4
tolujana <tolujana@gmail.com>
2024-10-27 22:59:31 +0100

    clean up and edited background task

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import PushNotification from 'react-native-push-notification';
-import configureBackgroundFetch from './src/util/BackgroundTask';
+// import configureBackgroundFetch from './src/util/BackgroundTask';
 import BackgroundFetch from 'react-native-background-fetch';
 AppRegistry.registerComponent(appName, () => App);
 

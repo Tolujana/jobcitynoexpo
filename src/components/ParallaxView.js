8c82aa3e867a1cbe18ea8804e714da1aa4e3bcce
tolujana <tolujana@gmail.com>
2024-11-07 21:59:24 +0100

    added banner ads between test and justified text

--- src/components/ParallaxView.js
+++ src/components/ParallaxView.js
@@ -14,9 +14,21 @@ import Animated, {
   interpolate,
 } from 'react-native-reanimated';
 import RenderHtml from 'react-native-render-html';
+import BannerAdComponent from './BannerAdComponent';
 const {width: screenWidth, height: screenHeight} = Dimensions.get('window');
 
 const ParallaxView = ({content, mainContent, image}) => {
+  // Find the first <b> tag in the article text
+
+  const boldTagIndex =
+    mainContent.indexOf('<strong>') || mainContent.indexOf('<b>');
+
+  // Split the article into two parts: before and after the first <b> tag
+  const beforeBoldText =
+    boldTagIndex !== -1 ? mainContent.slice(0, boldTagIndex) : mainContent;
+  const afterBoldText =
+    boldTagIndex !== -1 ? mainContent.slice(boldTagIndex) : '';
+
   const scrollY = useSharedValue(0);
 
   const scrollHandler = useAnimatedScrollHandler(event => {
@@ -39,7 +51,8 @@ const ParallaxView = ({content, mainContent, image}) => {
       backgroundColor: 'ffffff', // Change background color
       color: '#000', // Change text color
       fontFamily: 'Arial', // Change font family
-      padding: 10,
+      //padding: 10,
+      textAlign: 'justify',
     },
     p: {
       color: '#333', // Paragraph text color
@@ -82,11 +95,29 @@ const ParallaxView = ({content, mainContent, image}) => {
           {content}
         </Animated.View>
         <View style={{width: screenWidth * 0.95}}>
+          <BannerAdComponent />
+
+          {/* Start of Article (Before First <b> Tag) */}
           <RenderHtml
-            source={{html: mainContent}}
+            contentWidth={screenWidth * 0.95}
+            source={{html: beforeBoldText}}
             tagsStyles={tagsStyles}
-            //renderersProps={renderersProps}
+            baseStyle={{paddingHorizontal: 16, marginVertical: 10}}
           />
+
+          {/* Ad Just Before the First <b> Tag */}
+          {boldTagIndex !== -1 && <BannerAdComponent />}
+
+          {/* Rest of the Article (Including the <b> Tag and After) */}
+          <RenderHtml
+            contentWidth={screenWidth * 0.95}
+            source={{html: afterBoldText}}
+            tagsStyles={tagsStyles}
+            baseStyle={{paddingHorizontal: 16, marginVertical: 10}}
+          />
+
+          {/* End Ad */}
+          <BannerAdComponent />
         </View>
       </Animated.ScrollView>
     </View>

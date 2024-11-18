2ca9884feeec6f7590611eba64398a78d3bfe7ad
tolujana <tolujana@gmail.com>
2024-11-18 19:47:35 +0100

    added functionality for search and category, hence an update category cards and newlisting function

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,42 +5,79 @@ import {
   TextInput,
   ScrollView,
   StatusBar,
+  StyleSheet,
+  Alert,
 } from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 //mport { StatusBar } from "expo-status-bar";
 import {useColorScheme} from 'nativewind';
 //import {FontAwesome} from '@expo/vector-icons';
-import {useNavigation} from '@react-navigation/native';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
 import CategoriesCard from '../components/CategoriesCard';
 import {SpecializationList} from '../constants/categories';
-import useCustomFetch from '../util/Functions';
+import useCustomFetch from '../util/CustomFetch';
 import JobListing from './JobListing';
 import Listing from './Listing';
 import {AppContext} from '../context/AppContext';
 import NewListing from './NewListing';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ApiUrlManager from '../components/ApiUrlManager';
+import {loadSelectedspecialization, loadSelectedTopics} from '../util/funtions';
 
 export default function Home() {
   const navigation = useNavigation();
 
-  const [activeCategory, setActiveCategory] = useState(0);
+  const [activeCategory, setActiveCategory] = useState(null);
   const [jobEntries, setJobEntries] = useState([]);
   const {colorScheme, toggleColorScheme} = useColorScheme();
   const [key, setKey] = useState(0);
-
-  const handleCategoryChange = id => {
-    setActiveCategory(id);
-    // const url = `http://public-api.wordpress.com/rest/v1.2/sites/en.blog.wordpress.com/posts/?category=${specilization[id]}`;
-    // const params = { page: 1, number: 7 };
-    // console.log(url);
-    // fetchData(url, params);
-    // console.log(specilization[id].title, "home things");
+  const [specialization, setspecilization] = useState([]);
+  const [keywords, setKeywords] = useState([]);
+  const [sortedkeywords, setSortedKeywords] = useState([]);
+  const [sortedSpec, setsortedSpec] = useState([]);
+  const handleCategoryChange = category => {
+    setActiveCategory(category);
   };
 
-  useEffect(() => {}, []);
+  // useEffect(() => {
+  //   loadSelectedspecialization(setspecilization);
+  //   loadSelectedspecialization(setKeywords, 'apiList');
+  // }, []);
 
+  useFocusEffect(
+    React.useCallback(() => {
+      loadSelectedspecialization(setspecilization);
+      loadSelectedspecialization(setKeywords, 'apiList');
+    }, []),
+  );
+
+  useEffect(() => {
+    setsortedSpec(specialization.sort((a, b) => a.name.localeCompare(b.name)));
+    setActiveCategory(sortedSpec[0]);
+  }, [specialization]);
+  useEffect(() => {
+    setSortedKeywords(keywords.sort((a, b) => a.search.localeCompare(b.name)));
+  }, [keywords]);
+
+  const addMenuItems = () => {
+    Alert.alert('Choose Menu type', '', [
+      {
+        text: 'Cancel',
+        style: 'cancel',
+      },
+      {
+        text: 'Add Specialization',
+        onPress: () => navigation.navigate('Categories'),
+      },
+      {
+        text: 'Add Custom Menu(keyword)',
+        onPress: () => navigation.navigate('Form'),
+      },
+    ]);
+  };
+
+  const fullMenu = [...sortedSpec, ...sortedkeywords];
   return (
     <SafeAreaView className="pt-8 flex-1 bg-white dark:bg-neutral-900">
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
@@ -71,28 +108,69 @@ export default function Home() {
 
       {/* categories */}
       <View className=" flex-row my-4 ml-2">
+        {/* plus button  */}
+        {fullMenu.length > 0 && (
+          <TouchableOpacity style={styles.button} onPress={addMenuItems}>
+            <Icon name="plus" size={24} color="#fff" />
+          </TouchableOpacity>
+        )}
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
           className="space-x-4"
           contentContainerStyle={{paddingRight: 20}}>
-          {SpecializationList.map((category, index) => {
-            return (
-              <CategoriesCard
-                category={category}
-                index={index}
-                key={index}
-                activeCategory={activeCategory}
-                onPress={() => handleCategoryChange(index)}
-              />
-            );
-          })}
+          {/* category */}
+          {fullMenu.length > 0 ? (
+            fullMenu.map((category, index) => {
+              return (
+                <CategoriesCard
+                  category={category}
+                  index={index}
+                  key={index}
+                  activeCategory={activeCategory || sortedSpec[0]}
+                  onPress={() => handleCategoryChange(category)}
+                />
+              );
+            })
+          ) : (
+            <TouchableOpacity style={styles.button} onPress={addMenuItems}>
+              <View className=" flex-row">
+                <Icon name="plus" size={24} color="#fff" />
+                <Text>Click here to add menu items</Text>
+              </View>
+            </TouchableOpacity>
+          )}
         </ScrollView>
       </View>
       {/* jobLists */}
       <View>
-        <NewListing category={SpecializationList[activeCategory]} />
+        {!activeCategory && (
+          <NewListing
+            category={sortedSpec[0] || {name: 'All jobs', slug: ''}}
+          />
+        )}
+        {activeCategory?.name ? (
+          <NewListing category={activeCategory} />
+        ) : (
+          <NewListing category={null} search={activeCategory} />
+        )}
       </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    backgroundColor: '#4CAF50',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+});

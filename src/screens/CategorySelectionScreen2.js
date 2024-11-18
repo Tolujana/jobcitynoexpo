9ca4aaa7481340359e1ff70a29ff6d1c0c424602
tolujana <tolujana@gmail.com>
2024-11-18 19:50:44 +0100

    specialization now updated every 48hrs from the cloud

--- src/screens/CategorySelectionScreen2.js
+++ src/screens/CategorySelectionScreen2.js
@@ -5,83 +5,116 @@ import {
   TouchableOpacity,
   ScrollView,
   StyleSheet,
+  Alert,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
-import {SpecializationList} from '../constants/categories';
+import {NewSpec, SpecializationList} from '../constants/categories';
 import CheckBox from '@react-native-community/checkbox';
+import {
+  checkAndFetchData,
+  loadSelectedspecialization,
+  loadSelectedTopics,
+} from '../util/funtions';
 
-const topics = SpecializationList;
 const CategorySelectionScreen2 = () => {
   const [selectedTopics, setSelectedTopics] = useState([]);
 
+  const [data, setData] = useState(NewSpec);
+  const [loading, setLoading] = useState(true);
+
+  const loadData = async () => {
+    setLoading(true);
+    const fetchedData = await checkAndFetchData();
+
+    setData(fetchedData);
+    setLoading(false);
+  };
+
   // Load previously selected topics from AsyncStorage when the component mounts
   useEffect(() => {
-    const loadSelectedTopics = async () => {
-      try {
-        const storedTopics = await AsyncStorage.getItem('userTopics');
-        if (storedTopics) {
-          setSelectedTopics(JSON.parse(storedTopics));
-        }
-      } catch (error) {
-        console.error('Failed to load topics:', error);
-      }
-    };
-    loadSelectedTopics();
+    loadData();
+    loadSelectedspecialization(setSelectedTopics);
   }, []);
 
   // Save selected topics to AsyncStorage whenever they change
-  useEffect(() => {
-    const saveSelectedTopics = async () => {
-      try {
-        await AsyncStorage.setItem(
-          'userTopics',
-          JSON.stringify(selectedTopics),
-        );
-      } catch (error) {
-        console.error('Failed to save topics:', error);
-      }
-    };
-    saveSelectedTopics();
-  }, [selectedTopics]);
+  const saveSelectedTopics = async () => {
+    try {
+      await AsyncStorage.setItem(
+        'userTopics',
+        JSON.stringify(selectedTopics),
+      ).then(() => {
+        Alert.alert('Success', 'Preference saved successfully');
+      });
+    } catch (error) {
+      console.error('Failed to save topics:', error);
+    }
+  };
+
+  // useEffect(() => {
+  //   saveSelectedTopics();
+  // }, [selectedTopics]);
+
+  //exclude categories list
+  const excludedNames = [
+    'Uncategorized',
+    'Joblist',
+    'Jobberman',
+    'Hotnigerianjobs',
+    'Jobgurus',
+    'Myjobmag',
+  ];
 
   // Handle topic subscription/unsubscription
   const handleTopicToggle = async topic => {
+    const {name, slug} = topic;
     let updatedTopics = [];
-    if (selectedTopics.includes(topic)) {
+    if (selectedTopics.some(t => t.slug === topic.slug)) {
       // Unsubscribe from topic if already selected
-      await messaging().unsubscribeFromTopic(topic);
-      updatedTopics = selectedTopics.filter(t => t !== topic);
+      await messaging().unsubscribeFromTopic(slug);
+      updatedTopics = selectedTopics.filter(t => t.slug !== topic.slug);
     } else {
       // Subscribe to topic if not already selected
-      await messaging().subscribeToTopic(topic);
+      await messaging().subscribeToTopic(slug);
       updatedTopics = [...selectedTopics, topic];
     }
     setSelectedTopics(updatedTopics);
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Select Topics to Subscribe</Text>
-      {topics.map((topic, index) => (
-        <View key={index} style={styles.topicContainer}>
-          <TouchableOpacity onPress={() => handleTopicToggle(topic)}>
-            {/* <View style={styles.checkbox}>
+    <>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.title}>Select Topics to Subscribe</Text>
+        {data
+          .filter(topic => !excludedNames.includes(topic.name))
+          .map((topic, index) => (
+            <View key={index} style={styles.topicContainer}>
+              <TouchableOpacity onPress={() => handleTopicToggle(topic)}>
+                {/* <View style={styles.checkbox}>
               {selectedTopics.includes(topic) && (
                 <View style={styles.checked} />
               )}
             </View> */}
 
-            <CheckBox
-              tintColors={{true: '#2563eb'}}
-              value={selectedTopics.includes(topic) || false}
-              onValueChange={() => handleTopicToggle(topic)}
-            />
-          </TouchableOpacity>
-          <Text style={styles.topicText}>{topic}</Text>
-        </View>
-      ))}
-    </ScrollView>
+                <CheckBox
+                  tintColors={{true: '#2563eb'}}
+                  value={
+                    selectedTopics.some(t => t.slug === topic.slug) || false
+                  }
+                  onValueChange={() => handleTopicToggle(topic)}
+                />
+              </TouchableOpacity>
+              <Text style={styles.topicText}>{topic.name}</Text>
+            </View>
+          ))}
+      </ScrollView>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={saveSelectedTopics}
+        activeOpacity={0.8}>
+        <Text style={styles.buttonText}>Save Preferences</Text>
+      </TouchableOpacity>
+    </>
   );
 };
 const styles = StyleSheet.create({
@@ -114,6 +147,25 @@ const styles = StyleSheet.create({
   },
   topicText: {
     fontSize: 16,
+  },
+  button: {
+    marginHorizontal: 20,
+    marginBottom: 10,
+    backgroundColor: '#4CAF50',
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 

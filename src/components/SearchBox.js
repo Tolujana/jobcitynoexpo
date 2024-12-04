fb4377ad3f225d879525baa3f7fac672153828dc
tolujana <tolujana@gmail.com>
2024-12-04 22:12:46 +0100

    added the searchbox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,11 +2,12 @@ import {View, Text, TextInput, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import React, {useState} from 'react';
 
-export default function SearchBox() {
+export default function SearchBox({search}) {
   const [focused, setFocused] = useState(false);
+  const [searchText, setSearchText] = useState('');
   return (
     <View className="flex-row  justify-start items-center border border-gray-300 bg-neutral-100 dark:bg-neutral-800 rounded-full">
-      <TouchableOpacity className="p-2">
+      <TouchableOpacity className="pl-2">
         <Icon name="search" size={25} color="gray" />
       </TouchableOpacity>
       <TextInput
@@ -15,6 +16,8 @@ export default function SearchBox() {
         onFocus={() => setFocused(true)}
         onBlur={() => setFocused(false)}
         placeholderTextColor={'gray'}
+        value={search || searchText}
+        onChangeText={() => setSearchText(searchText)}
         //onPress={() => navigation.navigate('Search Jobs')}
       />
     </View>

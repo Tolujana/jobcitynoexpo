8a174ba654fa621f8f6431a464eaf2a774d4a3ae
tolujana <tolujana@gmail.com>
2024-12-23 23:20:46 +0100

    udated theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,11 +4,16 @@ const lightTheme = {
     secondary: '#f79511',
     tertiary: '#8686AC',
     background: '#f6f6f6',
+    background2: '#f9f9f9',
+    transBackground: 'rgba(246, 246, 246 , 0.8)',
     //background: '#f6f6f6', // Light gray
     text: '#000000', // Black text
     text2: '#ffffff  ',
     //surface: '#ffffff', // White surface
     error: '#B00020', // Error red
+    lightGrey: '#EBEDEF',
+    darkGrey: '#ccd1d1',
+    boldText: '#eb5615',
   },
   roundness: 8, // Adjust the corner radius for all components
 };
@@ -18,11 +23,16 @@ const darkTheme = {
     primary: '#f79511', // orange
     secondary: '#272757',
     tertiary: '#8686AC', // Teal
-    background: '"0f0f2d', // black
+    background: '#0f0f2d', // black
+    background2: '#0f0f37', // black
+    transBackground: 'rgba(15, 15, 45, 0.8)',
     text: '#ffffff  ', // Black text
     text2: '#00000',
     surface: '#ffffff', // White surface
     error: '#B00020', // Error red
+    lightGrey: '#EBEDEF',
+    darkGrey: '#ccd1d1',
+    boldText: '#f79511',
   },
   roundness: 8, // Adjust the corner radius for all components
 };

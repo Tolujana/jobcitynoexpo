bbbae4e2dd9c953b5789494485207d006f83bde7
tolujana <tolujana@gmail.com>
2025-02-03 15:26:18 +0100

    updated reward context

--- src/context/RewardContext.js
+++ src/context/RewardContext.js
@@ -7,15 +7,15 @@ export const RewardProvider = ({children}) => {
   const [rewardPoints, setRewardPoints] = useState(0);
   const [prevPoints, setPrevPoints] = useState(0);
 
-  useEffect(() => {
-    if (rewardPoints > 0) {
-      Alert.alert(
-        'Congratulations!',
-        `You've earned ${rewardPoints} more points!`,
-      );
-    }
-    setPrevPoints(rewardPoints);
-  }, [rewardPoints]);
+  // useEffect(() => {
+  //   if (rewardPoints > 0) {
+  //     Alert.alert(
+  //       'Congratulations!',
+  //       `You've earned ${rewardPoints} more pointseeee!`,
+  //     );
+  //   }
+  //   setPrevPoints(rewardPoints);
+  // }, [rewardPoints]);
 
   return (
     <RewardContext.Provider value={{rewardPoints, setRewardPoints}}>
